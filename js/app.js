3342b657469bc08e0828616bf7617eaaad3997a7
Jared Hyers <jared@laicos.com>
2015-08-23 15:10:32 -0400

    Angular research

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-var viewModel = function(){
+angular.module('todoApp', [])
+   .controller('todoCtrl', function($scope) {
 
+   $scope.totalTodos = 4;
+
+   $scope.todos=[
+      {text:'Learn AngularJS', done:false},
+      {text:'This guy types too fast', done:false},
+      {text:'Now that it is working, make it better.', done:false}
+   ];
    
-
-}
-
-ko.applyBindings(new viewModel());
+});
