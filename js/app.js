1b395aa75510e34512b4ffc058266d5dc028e2ed
Jared Hyers <jared@laicos.com>
2015-08-24 11:08:56 -0400

    MAKE IT WORK

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 angular.module('todoApp', [])
    .controller('todoCtrl', function($scope) {
 
-   $scope.totalTodos = 4;
-
    $scope.todos=[
       {text:'Learn AngularJS', done:false},
       {text:'This guy types too fast', done:false},
       {text:'Now that it is working, make it better.', done:false}
    ];
-   
-});
+   $scope.getTotalTodos = function(){
+      return $scope.todos.length;
+   }
+
+   $scope.addTodo = function(){
+      $scope.todos.push({text:$scope.formTodoText, done:false});
+      $scope.formTodoText = '';
+   };
+
+   $scope.clearCompleted = function(){
+      var oldTodos = $scope.todos;
+      $scope.todos=[];
+         angular.forEach(oldTodos, function(todo){
+            if (!todo.done) $scope.todos.push(todo);
+         });
+      };
+   });
